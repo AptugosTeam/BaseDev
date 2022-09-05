35a35869af4957b0bc72e520fe41a3215fb93b32
Gaston Gorosterrazu <gastonius@gmail.com>
2022-09-05 15:29:50 -0300

    Import CSV Structure Update

--- structures/ImportCSVintoaTable/init.js
+++ structures/ImportCSVintoaTable/init.js
@@ -1,44 +1,34 @@
 Parameters.unique_id = aptugo.generateID()
 Parameters.fields = []
+Parameters.Name = aptugo.friendly(Parameters.Name).toLowerCase()
 
 const untitledTables = Application.tables.filter(table => table.name.substr(0,8) === 'Untitled').length
-const name = aptugo.friendly(Parameters.Name).toLowerCase()
-const singleName = `${aptugo.friendly(Parameters.Name).toLowerCase()}record`
+const singleName = `${Parameters.Name}record`
 
-const readUploadedFileAsText = () => {
-  var reader = new FileReader()
-  return new Promise((resolve, reject) => {
-    reader.onload = function(e) {
-      var contents = e.target.result
-      var finalFields = []
-      var lines = this.result.split('\n')
-      var headers = lines.shift()
-      var fields = headers.split(',')
-      fields.forEach(field => {
-        Parameters.fields.push(aptugo.friendly(field))
-        finalFields.push({
-          column_name: aptugo.friendly(field),
-          data_type: "String",
-          unique_id: aptugo.generateID()
-        })
-      })
-
-      const newTable = {
-        type: 'table',
-        unique_id: Parameters.unique_id,
-        name: name,
-        singleName: singleName,
-        subtype: 'Aptugo',
-        children: [],
-        fields: finalFields
-      }
-      resolve(newTable)
-    }
-    reader.readAsText(Parameters.csv)
+var contents = aptugo.readFile(Parameters.csv)
+var finalFields = []
+var lines = contents.split('\n')
+var headers = lines.shift()
+var fields = headers.split(',')
+fields.forEach(field => {
+  Parameters.fields.push(aptugo.friendly(field))
+  finalFields.push({
+    column_name: aptugo.friendly(field),
+    data_type: "String",
+    unique_id: aptugo.generateID()
   })
+})
+
+const newTable = {
+  type: 'table',
+  unique_id: Parameters.unique_id,
+  name: Parameters.Name,
+  singleName: singleName,
+  subtype: 'Aptugo',
+  children: [],
+  fields: finalFields
 }
 
-const newTable = await readUploadedFileAsText() 
 if (!Application.tables) Application.tables = []
 Application.tables.push(newTable)
 return Application
