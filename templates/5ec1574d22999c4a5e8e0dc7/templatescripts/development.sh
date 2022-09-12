3731bd311d98db39f382f89856bb47e41eada970
Gaston Gorosterrazu <gastonius@gmail.com>
2022-09-12 16:20:14 -0300

    Update Packages Requirements

--- templates/5ec1574d22999c4a5e8e0dc7/templatescripts/development.sh
+++ templates/5ec1574d22999c4a5e8e0dc7/templatescripts/development.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-if [ "$USE_PNPM" == "false" ]
-then
-  echo 'Starting NPM Install'
-  npm install
-  echo 'Finished NPM Install'
-else
-  export NODE_ENV=development
-  echo 'Starting PNPM Install'
-  pnpm install --prefer-offline
-  echo 'Finished PNPM Install'
-fi
+export NODE_ENV=development
+echo 'Starting PNPM Install'
+pnpm install --prefer-offline
+echo 'Finished PNPM Install'
+
 
