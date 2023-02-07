84060503468749e40de8be2cd6dd015c7d1debd3
emaretamozo <elneretamozo@gmail.com>
2023-02-07 13:56:37 -0300

    2.0

--- templates/5ec1574d22999c4a5e8e0dc7/elements/Experimental/tanstackTable.tpl
+++ templates/5ec1574d22999c4a5e8e0dc7/elements/Experimental/tanstackTable.tpl
@@ -106,9 +106,127 @@ settings:
   - name: Packages
     value: '"react-dom": "^18.2.0",'
 */
-
-{% set bpr %}
+{% set editProc = element.values.editProcedure|default('No') %}
+{% set allowEdit = element.values.allowEdit|default(true) %}
+{% set allowDeletion = element.values.allowDeletion|default(true) %}
+{% set tableFields = [] %}
+{% if element.values.table == 'var' %}
+  {% set table = element.values.editionTable | tableData %}
+  {% set tableName = table.name | friendly %}
+  {% set tableSingleName = table.singleName | friendly | capitalize %}
+  {% set setEditDataFunctionName = 'set' ~ tableName ~ 'Data' %}
+  {% for field in element.children %}
+    {% if field.values.Field == 'useVar' %}
+      {% set tableFields = tableFields|merge([field.values.columnName]) %}
+    {% else %}
+      {% set currentField = field.values.Field | fieldData  %}
+      {% set tableFields = tableFields|merge([currentField.column_name]) %}
+    {% endif %}
+  {% endfor %}
+  {% set tableData = element.values.variableToUse %}
+{% else %}
+  {% set table = element.values.table | tableData %}
+  {% set tableName = table.name | friendly %}
+  {% set tableSingleName = table.singleName | friendly | capitalize %}
+  {% set setEditDataFunctionName = 'set' ~ tableName ~ 'Data' %}
+  {% set fields = table.fields %}
+  {% set innervarname = element.name | friendly %}
+  {% set eleWithoutChilds = element %}
+  {% set eleWithoutChilds = eleWithoutChilds|merge({'children': null}) %}
+  {% include includeTemplate('loadFromRedux.tpl') with { 'data': element.values.table, 'element': eleWithoutChilds, 'defaultPage': element.values.defaultPage } %}
+  {% if element.children %}
+      {% for field in element.children %}
+        {% if field.values.Field == 'useVar' %}
+          {% set tableFields = tableFields|merge([field.values.columnName]) %}
+        {% else %}
+          {% set currentField = field.values.Field | fieldData  %}
+          {% set tableFields = tableFields|merge([currentField.displaylabel|default(currentField.column_name)]) %}
+        {% endif %}
+      {% endfor %}
+  {% else %}
+      {% set fields = table.fields %}
+      {% for field in fields %}
+          {% set tableFields = tableFields|merge([field.displaylabel|default(field.column_name)]) %}
+      {% endfor %}
+  {% endif %}
+  {% set tableData = '(' ~ table.name|friendly|lower ~ 'Data.found' ~ table.name|friendly|lower ~ '.length ? ' ~ table.name|friendly|lower ~ 'Data.found' ~ table.name|friendly|lower ~ ' : ' ~ table.name|friendly|lower ~ 'Data.' ~ table.name|friendly|lower ~ ' as any)' %}
+  {% set bpr %}
+  import { add{{ table.name | friendly | capitalize }}, load{{ table.name | friendly | capitalize }}, remove{{ table.singleName | friendly | capitalize }}, edit{{ table.name | friendly | capitalize }} } from '../store/actions/{{ table.name | friendly | lower }}Actions'
   import Tanstack from '../components/TanstackTable/Tanstack'
-{% endset %}
-{{ save_delayed('bpr', bpr ) }}
-<Tanstack><div>Hola</div></Tanstack>
+  import style from '../components/TanstackTable/index.module.scss'
+  import EditIcon from '@mui/icons-material/Edit'
+  import DeleteIcon from '@mui/icons-material/Delete'
+  import IconButton from '@mui/material/IconButton'
+  import MoreIcon from '@mui/icons-material/More'
+  {% endset %}
+  {{ save_delayed('bpr', bpr ) }}
+{% endif %}
+<Tanstack
+    tableHead={
+      {% if element.values.headerVariable %}
+        {{element.values.headerVariable}}
+      {% else %}
+        [{% for field in tableFields %}"{{ field }}",{% endfor %}{% if editProc != 'No' or allowEdit or allowDeletion %}"Actions"{% endif %}]
+      {% endif %}
+    }
+    tableData={ {{ tableData }} }
+    {% if element.values.table != 'var' %}
+      orderBy={ {{ innervarname }}loadoptions.sort.field }
+      order={ {{ innervarname }}loadoptions.sort.method }
+      onRequestSort={(event, property) => {
+        set{{ innervarname }}loadoptions({
+          ...{{ innervarname }}loadoptions,
+          sort: {
+            field: property,
+            method: {{ innervarname }}loadoptions.sort.field === property ? ({{ innervarname }}loadoptions.sort.method === 'asc' ? 'desc' : 'asc') : 'ASC',
+          }
+        })
+      }}
+    {% endif %}
+    >
+{% if editProc != 'No' %}
+<div className={style.actionsArea}>
+    {% if element.values.detailsURL and element.values.detailsURL != 'No' %}
+    <IconButton
+      aria-label="edit"
+      color="primary"
+      onClickCapture={(e: any) => { 
+        const url = '{{ (element.values.detailsURL | elementData ).path }}'.replace(':id', e.element._id)
+        props.history.push(url)
+      }}
+    >
+      <MoreIcon fontSize="small" />
+    </IconButton>
+    {% endif %}
+    {% if allowEdit %}
+    <IconButton
+      aria-label="edit"
+      color="primary"
+      onClickCapture={(e: any) => { 
+        {% if editProc == 'Internal' %}
+          {{ setEditDataFunctionName }}(e.element)
+          setdialog{{ tableName | capitalize }}Action('edit')
+        {% else %}
+          const url = '{{ (editProc | elementData ).path }}'.replace(':id', e.element._id)
+          props.history.push(url)
+        {% endif %}
+      }}
+    >
+      <EditIcon fontSize="small" />
+    </IconButton>
+    {% endif %}
+    {% if allowDeletion %}
+    <IconButton aria-label="delete" color="primary" onClickCapture={(e: any) => {
+      {% if element.values.confirmDeletes %}
+        {{ setEditDataFunctionName }}(e.element)
+        setdialog{{ tableName | capitalize }}Action('delete')
+      {% else %}
+        dispatch(remove{{ tableSingleName }} (e.element))
+      {% endif %}
+    }}>
+      <DeleteIcon fontSize="small" />
+    </IconButton>
+    {% endif %}
+</div>
+{% endif %}
+</Tanstack>
--- templates/5ec1574d22999c4a5e8e0dc7/front-end/components/TanstackTable/Tanstack.tsx
+++ templates/5ec1574d22999c4a5e8e0dc7/front-end/components/TanstackTable/Tanstack.tsx
@@ -1,50 +1,105 @@
-/*
-path: Tanstack.tsx
-completePath: front-end/components/TanstackTable/Tanstack.tsx
-unique_id: KaVZpxkg
-*/
-
+import { createColumnHelper, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table'
 import React, { FunctionComponent } from 'react'
 import style from './index.module.scss'
-import { createColumnHelper, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table'
 
-type Person = {
-  firstName: string
-  lastName: string
-  age: number
-  visits: number
-  status: string
-  progress: number
+interface tableProps {
+  addProcedure?: Function
+  addTitle?: string
+  addText?: string
+  tableData: Array<Array<string>> | Array<any>
+  tableHead: string[]
+  orderBy?: string
+  order?: 'asc' | 'desc'
+  onRequestSort?: Function
 }
 
-const defaultData: Person[] = [
-  {
-    firstName: 'tanner',
-    lastName: 'linsley',
-    age: 24,
-    visits: 100,
-    status: 'In Relationship',
-    progress: 50,
-  },
-  {
-    firstName: 'tandy',
-    lastName: 'miller',
-    age: 40,
-    visits: 40,
-    status: 'Single',
-    progress: 80,
-  },
-  {
-    firstName: 'joe',
-    lastName: 'dirte',
-    age: 45,
-    visits: 20,
-    status: 'Complicated',
-    progress: 10,
-  },
-]
+const TableHeader = (tableProps) => {
+  const { tableHead, orderBy, order, onRequestSort } = tableProps
+  const createSortHandler = (property) => (event: React.MouseEvent<unknown>) => {
+    onRequestSort(event, property)
+  }
 
-const columnHelper = createColumnHelper<Person>()
+  const renderHeader = (headCell) => {
+    if (!onRequestSort) return headCell
+
+    return (
+      <TableSortLabel
+        active={orderBy === headCell}
+        direction={orderBy === headCell ? order : 'asc'}
+        onClick={createSortHandler && createSortHandler(headCell)}
+      >
+        {headCell}
+      </TableSortLabel>
+    )
+  }
+
+  return (
+    <thead>
+      {tableHead.getHeaderGroups().map((headerGroup) => (
+        <tr key={headerGroup.id}>
+          {headerGroup.headers.map((header) => (
+            <th key={header.id}>{header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}</th>
+          ))}
+        </tr>
+      ))}
+    </thead>
+  )
+}
+const TableBody = (tableProps) => {
+  const { tableData } = tableProps
+  const createSortHandler = (property) => (event: React.MouseEvent<unknown>) => {
+    onRequestSort(event, property)
+  }
+
+  return (
+    <tbody>
+      {tableData.getRowModel().rows.map((row) => (
+        <tr key={row.id}>
+          {row.getVisibleCells().map((cell) => (
+            <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  )
+}
+// type Person = {
+//   firstName: string
+//   lastName: string
+//   age: number
+//   visits: number
+//   status: string
+//   progress: number
+// }
+
+// const defaultData: Person[] = [
+//   {
+//     firstName: 'tanner',
+//     lastName: 'linsley',
+//     age: 24,
+//     visits: 100,
+//     status: 'In Relationship',
+//     progress: 50,
+//   },
+//   {
+//     firstName: 'tandy',
+//     lastName: 'miller',
+//     age: 40,
+//     visits: 40,
+//     status: 'Single',
+//     progress: 80,
+//   },
+//   {
+//     firstName: 'joe',
+//     lastName: 'dirte',
+//     age: 45,
+//     visits: 20,
+//     status: 'Complicated',
+//     progress: 10,
+//   },
+// ]
+
+const columnHelper = createColumnHelper()
 
 const columns = [
   columnHelper.accessor('firstName', {
@@ -76,185 +131,53 @@ const columns = [
   }),
 ]
 
-
-
 const Tanstack: FunctionComponent<tableProps> = (props) => {
-    const [data, setData] = React.useState(() => [...defaultData])
-    const rerender = React.useReducer(() => ({}), {})[1]
+  const [data, setData] = React.useState(() => [...props.tableData])
+  const rerender = React.useReducer(() => ({}), {})[1]
 
-    const table = useReactTable({
-      data,
-      columns,
-      getCoreRowModel: getCoreRowModel(),
-    })
+  const table = useReactTable({
+    data,
+    getCoreRowModel: getCoreRowModel(),
+  })
 
+  // table.getHeaderGroups().map((headerGroup) => console.log(headerGroup))
 
-
-    return (
-      <div >
-        <table className={style.tabla}>
-          <thead>
-            {table.getHeaderGroups().map(headerGroup => (
-              <tr key={headerGroup.id}>
-                {headerGroup.headers.map(header => (
-                  <th key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody>
-            {table.getRowModel().rows.map(row => (
-              <tr key={row.id}>
-                {row.getVisibleCells().map(cell => (
-                  <td key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-          <tfoot>
-            {table.getFooterGroups().map(footerGroup => (
-              <tr key={footerGroup.id}>
-                {footerGroup.headers.map(header => (
-                  <th key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                        header.column.columnDef.footer,
-                        header.getContext()
-                      )}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </tfoot>
-        </table>
-        <button onClick={() => rerender()}>
-          Rerender
-        </button>
-      </div>
-    )
+  return (
+    <div>
+      <table className={style.tabla}>
+        {props.tableHead !== undefined ? <TableHeader {...props} /> : null}
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id}>{header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        {props.tableData !== undefined ? <TableBody {...props} /> : null}
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          {table.getFooterGroups().map((footerGroup) => (
+            <tr key={footerGroup.id}>
+              {footerGroup.headers.map((header) => (
+                <th key={header.id}>{header.isPlaceholder ? null : flexRender(header.column.columnDef.footer, header.getContext())}</th>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+      <button onClick={() => rerender()}>Rerender</button>
+    </div>
+  )
 }
 
 export default Tanstack
-
-// import Table from '@mui/material/Table'
-// import TableBody from '@mui/material/TableBody'
-// import TableCell from '@mui/material/TableCell'
-// import TableHead from '@mui/material/TableHead'
-// import TableSortLabel from '@mui/material/TableSortLabel'
-// import TableRow from '@mui/material/TableRow'
-// import React, { FunctionComponent } from 'react'
-// import styles from './table.module.scss'
-
-// interface tableProps {
-//   addProcedure?: Function
-//   addTitle?: string
-//   addText?: string
-//   tableData: Array<Array<string>> | Array<any>
-//   tableHead: string[]
-//   orderBy?: string
-//   order?: 'asc' | 'desc'
-//   onRequestSort?: Function
-// }
-
-// const TableHeader = (tableProps) => {
-//   const { tableHead, orderBy, order, onRequestSort } = tableProps;
-//   const createSortHandler = (property) => (event: React.MouseEvent<unknown>) => {
-//     onRequestSort(event, property)
-//   }
-
-//   const renderHeader = (headCell) => {
-//     if (!onRequestSort) return headCell
-
-//     return <TableSortLabel
-//       active={orderBy === headCell}
-//       direction={orderBy === headCell ? order : 'asc'}
-//       onClick={createSortHandler && createSortHandler(headCell)}
-//     >
-//       {headCell}
-//     </TableSortLabel>
-//   }
-
-//   return (
-//     <TableHead>
-//       <TableRow>
-//         {tableHead.map((headCell, key) => (
-//           <TableCell
-//             key={key}
-//             sortDirection={orderBy === headCell ? order : false}
-//           >
-//             {renderHeader(headCell)}
-//           </TableCell>
-//         ))}
-//       </TableRow>
-//     </TableHead>
-//   );
-// }
-
-// const AptugoTable: FunctionComponent<tableProps> = (props) => {
-//   const RenderCells = (cellData: React.ReactNode[]) => {
-//     let children = [props.children]
-//     try {
-//       children = [...(props.children as Array<any>)]
-//     } catch (e) {}
-
-//     return children.map((child: any, index) => {
-//       let newProps: any = { ...child.props }
-
-//       for (const [key, value] of Object.entries(newProps)) {
-//         newProps[key] = typeof value === 'function' ? value(cellData) : value
-//       }
-//       if (child.props.onClickCapture)
-//         newProps['onClickCapture'] = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-//           child.props.onClickCapture({ ...e, element: cellData })
-//         }
-
-//       newProps.children = React.Children.map(child.props.children, (subChild) => {
-//         let newSubProps: any = {}
-//         if (subChild.props.onClickCapture)
-//           newSubProps['onClickCapture'] = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-//             subChild.props.onClickCapture({ ...e, element: cellData })
-//           }
-//         return React.cloneElement(subChild, newSubProps)
-//       })
-
-//       const newChild = React.cloneElement(child, newProps)
-//       return <TableCell className={'tableCell'} key={`child${index}`}>{newChild}</TableCell>
-//     })
-//   }
-
-//   return (
-//     <div className={styles.tableHolder}>
-//       <Table className={styles.table}>
-//         {props.tableHead !== undefined ? <TableHeader {...props} /> : null}
-//         <TableBody>
-//           {props.tableData?.map((row, key) => {
-//             const fields = Object.values(row)
-//             return (
-//               <TableRow key={key} className={styles.tableBodyRow}>
-//                 {Array.isArray(props.children)
-//                   ? RenderCells(row)
-//                   : fields.map((field, subkey) => (
-//                       <TableCell className={styles.tableCell} key={subkey}>
-//                         {field}
-//                       </TableCell>
-//                     ))}
-//               </TableRow>
-//             )
-//           })}
-//         </TableBody>
-//       </Table>
-//     </div>
-//   )
-// }
-
-// export default AptugoTable
