a1b370dc098e078569a68dff41521c97b90bbcd2
Aptugo <72868791+Aptugo@users.noreply.github.com>
2023-02-08 14:52:07 -0300

    Some Native Fixes

--- templates/ReactNative/front-end/components/FileUpload/FileUpload.tsx
+++ templates/ReactNative/front-end/components/FileUpload/FileUpload.tsx
@@ -3,9 +3,11 @@ path: FileUpload.tsx
 completePath: front-end/components/FileUpload/FileUpload.tsx
 unique_id: QZm2nTtL
 */
+import React, { FunctionComponent } from 'react'
+import { Text } from 'react-native'
 
-const AddDialog: FunctionComponent = (props) => {
-  return 'table'
+const FileUpload: FunctionComponent = (props) => {
+  return <Text>FileUpload</Text>
 }
 
-export default AddDialog
+export default FileUpload
