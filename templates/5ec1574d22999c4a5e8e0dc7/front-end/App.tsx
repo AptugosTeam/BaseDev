07b3e932bb771b098b5ae093029c253ac3ba1a98
Gaston Gorosterrazu <gastonius@gmail.com>
2023-10-09 15:10:00 -0300

    Suspenso Mejor

--- templates/5ec1574d22999c4a5e8e0dc7/front-end/App.tsx
+++ templates/5ec1574d22999c4a5e8e0dc7/front-end/App.tsx
@@ -45,7 +45,7 @@ const App: React.FunctionComponent = (props: any) => {
 
   return (
     <React.Fragment>
-      <React.Suspense fallback={ {{ insert_setting('FallBack')|default('<span>Loading</span>') }} }>
+      <React.Suspense fallback={ {{ insert_setting('FallBack')|default('<span style={ { display: "flex", alignItems: "center", justifyContent: "center", height: "100%" } }>Loading</span>') }} }>
         {{ insert_setting('SiteWideWrapStart') | raw }}
         <React.Fragment>
           {switchRoutes}
