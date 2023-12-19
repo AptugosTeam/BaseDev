6738fea012ff1ed6ded7e4318a03888cbc9b3c0e
Albers <albers@aptugo.com>
2023-12-19 20:19:51 -0300

    Add Iconify element to ReactNative
    Añadí Iconify a ReactNative, tuve que modificar el babel.config.js para agregar el plugin :)

--- templates/ReactNative/front-end/babel.config.js
+++ templates/ReactNative/front-end/babel.config.js
@@ -7,6 +7,6 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ["transform-inline-environment-variables"]
+    plugins: ['transform-inline-environment-variables', 'react-native-iconify/plugin'],
   };
 };
