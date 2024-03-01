8af0c424022577cd2ca526a02029669380b63ad4
Robert Rondon <robrondon11@gmail.com>
2024-03-01 05:19:02 -0300

    Update 999_allowedOrigins.js

--- templates/5ec1574d22999c4a5e8e0dc7/elements/Experimental/AllowedOrigins/999_allowedOrigins.js
+++ templates/5ec1574d22999c4a5e8e0dc7/elements/Experimental/AllowedOrigins/999_allowedOrigins.js
@@ -1,3 +1,10 @@
+/*
+path: 999_allowedOrigins.js
+completePath: >-
+  /Users/robrondon/Aptugo/BaseDev/templates/5ec1574d22999c4a5e8e0dc7/elements/Experimental/AllowedOrigins/999_allowedOrigins.js
+keyPath: elements/Experimental/AllowedOrigins/999_allowedOrigins.js
+unique_id: Vrpp9Mbv
+*/
 const verifyOrigin = (req, res, next) => {
   // Temporalmente desactivado hasta resolver el insert_setting, 
   // la funcion se mueve al tpl
