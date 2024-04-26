7affa075e69d20268acffa7fdeb8f3740b3f5e32
Robert Rondon <robrondon11@gmail.com>
2024-04-26 11:06:44 -0300

    Update database.config.js
    falto esto

--- templates/5ec1574d22999c4a5e8e0dc7/back-end/config/database.config.js
+++ templates/5ec1574d22999c4a5e8e0dc7/back-end/config/database.config.js
@@ -4,6 +4,9 @@ completePath: back-end/config/database.config.js
 unique_id: cTdff9kS
 */
 // Database connection string
+const dotenv = require('dotenv')
+dotenv.config({ path: `${__dirname}/config/.env.development` })
+
 module.exports = {
   url: `${process.env.MONGO_URL}`
 }
