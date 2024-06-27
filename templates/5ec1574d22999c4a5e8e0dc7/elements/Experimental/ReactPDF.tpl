99edeccc79ee5d691137f54bb0f6293dfaddd711
MartineBordon26 <martinebordon26@gmail.com>
2024-06-27 19:55:00 -0300

    Update ReactPDF.tpl/tsx

--- templates/5ec1574d22999c4a5e8e0dc7/elements/Experimental/ReactPDF.tpl
+++ templates/5ec1574d22999c4a5e8e0dc7/elements/Experimental/ReactPDF.tpl
@@ -18,6 +18,26 @@ options:
     display: Page Width
     type: text
     options: ''
+  - name: showActionPage
+    display: Show Action Page
+    type: checkbox
+    settings:
+      default: false
+  - name: buttonText
+    display: Text button
+    type: text
+    options: ''
+    settings:
+      propertyCondition: showActionPage
+      condition: true
+  - name: action
+    display: Action button
+    type: text
+    options: ''
+    settings:
+      propertyCondition: showActionPage
+      condition: true
+
 settings:
   - name: Packages
     value: '"react-pdf": "7.7.1", "browserify-zlib": "^0.2.0","buffer": "^6.0.3","process": "^0.11.10","stream-browserify": "^3.0.0","util": "^0.12.4",'
@@ -41,5 +61,14 @@ children: []
     {% endif %}
     {% if element.values.pageWidth %}
         pageWidth={ {{element.values.pageWidth}} }
+    {% endif %}
+    {% if element.values.showActionPage %}
+        showActionPage={ {{element.values.showActionPage}} }
+    {% endif %}
+    {% if element.values.buttonText %}
+        buttonText={ {{element.values.buttonText}} }
+    {% endif %}
+    {% if element.values.action %}
+        action={ {{element.values.action}} }
     {% endif %}
 />
--- templates/5ec1574d22999c4a5e8e0dc7/front-end/components/ReactPDF/ReactPDF.tsx
+++ templates/5ec1574d22999c4a5e8e0dc7/front-end/components/ReactPDF/ReactPDF.tsx
@@ -7,43 +7,88 @@ sourceType: typescript
 children: []
 */
 
-import React from 'react'
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Document, Page } from 'react-pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 
 function ReactPDF(props) {
-  const [numPages, setNumPages] = useState<number>();
-  const [pageNumber, setPageNumber] = useState<number>(1);
+  const [numPages, setNumPages] = useState<number>(0);
+  const [isActived, setisActived] = useState<Boolean>(props.showActionPage);
 
   function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
-    if (props.maxNumPages) {
-      props.maxNumPages === false ? setNumPages(numPages) : setNumPages(props.maxNumPages)
-    } else {
-      setNumPages(numPages)
-    }
+    setNumPages(numPages);
+  }
+
+
+  const maxPages = props.maxNumPages === false ? numPages : props.maxNumPages;
+
+  const estilo1 = {
+    position: 'relative' 
+  }
+  const estilo2 = {
+    filter: 'blur(10px)'
+  }
+  const estilo3 = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    background: 'rgba(255, 255, 255, 0.8)',
+    padding: '20px',
+    borderRadius: '10px',
+  }
+  const estilo4 = {
+    padding: '10px 20px',
+    fontSize: '15px',
+    fontWeight: 'bold',
+    color: '#fff',
+    backgroundColor: '#508acf',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+    transition: 'background-color 0.3s ease',
   }
 
   return (
     <div>
       <Document file={props.file} onLoadSuccess={onDocumentLoadSuccess}>
-        {
-            Array.apply(null, Array(numPages))
-            .map((x, i) => i + 1)
-            .map((page) => {
-                return (
-                    <Page
-                    pageNumber={page}
-                    renderTextLayer={false}
-                    renderAnotationLayer={false}
-                    width={props.pageWidth}
-                    />
-                )
-            })
-        }
+        {Array.apply(null, Array(maxPages))
+          .map((x, i) => i + 1)
+          .map((page) => (
+            <Page
+              key={`page-${page}`}
+              pageNumber={page}
+              renderTextLayer={false}
+              renderAnnotationLayer={false}
+              width={props.pageWidth}
+            />
+          ))}
+        {(props.maxNumPages !== false && numPages > props.maxNumPages && isActived)  && (
+          <div style={estilos1}>
+            <div style={estilos2}>
+              <Page
+                key={`page-${maxPages + 1}`}
+                pageNumber={maxPages + 1}
+                renderTextLayer={false}
+                renderAnnotationLayer={false}
+                width={props.pageWidth}
+              />
+            </div>
+            <div
+              style={estilos3}
+            >
+              <button onClick={() => props.action }
+              style={estilos4}
+              >
+                {props.buttonText}
+              </button>
+            </div>
+          </div>
+        )}
       </Document>
-      
     </div>
   );
 }
+
 export default ReactPDF;
