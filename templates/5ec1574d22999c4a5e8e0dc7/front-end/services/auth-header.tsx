0ee7109da90af39b78740798fc38aa4d152a503a
MartineBordon26 <martinebordon26@gmail.com>
2024-07-17 10:17:26 -0300

    auth-headers.tsx fixed

--- templates/5ec1574d22999c4a5e8e0dc7/front-end/services/auth-header.tsx
+++ templates/5ec1574d22999c4a5e8e0dc7/front-end/services/auth-header.tsx
@@ -6,19 +6,30 @@ unique_id: kvycd8W9
 import axios from "axios"
 
 const authHeaders = async () => {
-  const token = localStorage.getItem('user') && localStorage.getItem('token')  || sessionStorage.getItem('userSession') && sessionStorage.getItem('tokenSession') ||
-    (sessionStorage.getItem('user') && sessionStorage.getItem('token'));
+  let token = null;
+
+  if (localStorage.getItem('user') && localStorage.getItem('token')) {
+    token = localStorage.getItem('token');
+    console.log('Using token from localStorage');
+  } else if (sessionStorage.getItem('userSession') && sessionStorage.getItem('tokenSession')) {
+    token = sessionStorage.getItem('tokenSession');
+    console.log('Using token from sessionStorage userSession');
+  } else if (sessionStorage.getItem('user') && sessionStorage.getItem('token')) {
+    token = sessionStorage.getItem('token');
+    console.log('Using token from sessionStorage user');
+  }
+
   if (token) {
-      axios.defaults.headers.common['Authorization'] = token
-      return true
+    axios.defaults.headers.common['Authorization'] = token
+    return true
   } else {
-      axios.defaults.headers.common['Authorization'] = null
-      return false
-      /*if setting null does not remove `Authorization` header then try     
+    axios.defaults.headers.common['Authorization'] = null
+    return false
+    /*if setting null does not remove `Authorization` header then try     
         delete axios.defaults.headers.common['Authorization'];
       */
   }
-}
+};
 
 export default authHeaders
 
