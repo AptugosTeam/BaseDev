88a0d6a696373569a6e7e681587b9d85014f8b61
Aptugo <gaston@aptugo.com>
2024-10-23 12:20:23 -0300

    Listo el upload de games

--- templates/5ec1574d22999c4a5e8e0dc7/templatescripts/deploy.js
+++ templates/5ec1574d22999c4a5e8e0dc7/templatescripts/deploy.js
@@ -1,12 +1,13 @@
 const deploy = (parameters) => {
   return [{
-    folders: ['build'],
+    folders: ['build','dist'],
     url: parameters.settings.url.substring(8).toLowerCase(),
-    uploadURL: 'https://appuploader.aptugo.app:8501/upload?type=fe'
+    uploadURL: 'https://appuploader.aptugo.app:8500/upload?type=fe'
   },{
     folders: ['back-end'],
     url: parameters.settings.apiURL.substring(8).toLowerCase(),
-    uploadURL: 'https://appuploader.aptugo.app:8501/upload?type=be'
+    uploadURL: 'https://appuploader.aptugo.app:8500/upload?type=be'
   }]
 }
+
 module.exports = deploy
