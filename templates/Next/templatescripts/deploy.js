4756c0397715c96eda2a54f5245132f6f8472330
Aptugo <gaston@aptugo.com>
2025-01-16 14:06:04 -0300

    Deploys

--- templates/Next/templatescripts/deploy.js
+++ templates/Next/templatescripts/deploy.js
@@ -1,10 +1,7 @@
-const deploy = (parameters) => {
+
   return [{
     folders: ['.'],
     ignoreFolders: ['node_modules'],
     url: parameters.settings.url.substring(8).toLowerCase(),
     uploadURL: 'https://appcontroller.next.aptugo.app/api/apps/upload?type=fe'
-    // uploadURL: 'http://localhost:8012/api/apps/upload?type=fe'
   }]
-}
-module.exports = deploy
