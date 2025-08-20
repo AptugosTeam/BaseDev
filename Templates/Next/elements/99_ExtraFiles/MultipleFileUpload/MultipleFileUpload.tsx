3f3cef5e426b06972e03c5bf6ec962f01ce4131d
Carlos Daniel Olivera <68717228+Be7ruvio@users.noreply.github.com>
2025-08-19 22:44:46 -0300

    Update MultipleFileUpload.tsx
    - Add onMount
    - Add onChange
    - Add onCleanQueue
    - Add innerText

--- Templates/Next/elements/99_ExtraFiles/MultipleFileUpload/MultipleFileUpload.tsx
+++ Templates/Next/elements/99_ExtraFiles/MultipleFileUpload/MultipleFileUpload.tsx
@@ -6,14 +6,20 @@ keyPath: elements/99_ExtraFiles/MultipleFileUpload/MultipleFileUpload.tsx
 unique_id: ploiIy7s
 */
 
-import React, { FunctionComponent, useCallback } from 'react'
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
+import React, { FunctionComponent, useCallback } from 'react'
 import { useDropzone } from 'react-dropzone'
 
 const MultipleFileUpload: FunctionComponent<any> = (props) => {
   const [files, setFiles] = React.useState(props.files || [])
 
-  const onDrop = useCallback(acceptedFiles => {
+  React.useEffect(() => {
+    if (props.onMount && props.onMount.length > 0 && Object.keys(props.onMount[0]).length > 0) {
+      setFiles(props.onMount)
+    }
+  }, [props.onMount])
+
+  const onDrop = useCallback((acceptedFiles) => {
     const newFile = acceptedFiles.map((file) =>
       Object.assign(file, {
         id: `${file.name}-${file.lastModified}`,
@@ -22,40 +28,46 @@ const MultipleFileUpload: FunctionComponent<any> = (props) => {
     )
 
     setFiles([...files, ...newFile])
+    if (props.onChange) {
+      props.onChange([...files, ...newFile])
+    }
+    // console.log('Files:', [...files, ...newFile])
   }, [])
 
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+  const cleanQueue = () => {
+    setFiles([])
+    if (props.onCleanQueue) {
+      props.onCleanQueue()
+    }
+  }
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
   return (
     <section className={props.className}>
       <div {...getRootProps()}>
         <input {...getInputProps()} />
-        {
-          isDragActive ?
-            <p>Drop the files here ...</p> :
-            <p>Drag 'n' drop some files here, or click to select files</p>
-        }
+        {isDragActive ? <p>Drop the files here ...</p> : <p>{props.innerText || "Drag 'n' drop some files here, or click to select files"}</p>}
       </div>
       <aside className="thumbsContainer">
         {files.map((file, index) => {
-          return (<div
-            className="thumbnail"
-            // onClick={() => { setHighlight(file) }}
-          >
-          <DeleteForeverIcon
-            className="deleteIcon"
-            onClick={(e) => {
-              // removeImage(file, e)
-            }}
-          />
-          <div className="thumbnailImageContainer">
-            <img
-              className="thumbnailImage"
-              src={file.Image ? `/img/${file.Image}` : file.preview}
-              onClick={(e) => e.stopPropagation()}
-            />
-          </div>
-        </div>)
+          return (
+            <div
+              className="thumbnail"
+              // onClick={() => { setHighlight(file) }}
+            >
+              <DeleteForeverIcon
+                className="deleteIcon"
+                onClick={(e) => {
+                  cleanQueue()
+                  // removeImage(file, e)
+                }}
+              />
+              <div className="thumbnailImageContainer">
+                <img className="thumbnailImage" src={file.Image ? `/img/${file.Image}` : file.preview} onClick={(e) => e.stopPropagation()} />
+              </div>
+            </div>
+          )
         })}
       </aside>
     </section>
