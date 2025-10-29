133e9dbc1445912d09193cd0a833237bf88162de
Aptugo <gaston@aptugo.com>
2025-10-29 13:31:02 -0300

    Make Aptugo upload compatible with 1 server and haltugo, and others as well.

--- templates/5ec1574d22999c4a5e8e0dc7/templatescripts/deploy.js
+++ templates/5ec1574d22999c4a5e8e0dc7/templatescripts/deploy.js
@@ -1,12 +1,18 @@
 const deploy = (parameters) => {
   return [{
+    name: 'Upload Front-End',
     folders: ['build'],
+    ignoreFolders: ['node_modules'],
     url: parameters.settings.url.substring(8).toLowerCase(),
-    uploadURL: 'https://appuploader.aptugo.app:8501/upload?type=fe'
-  },{
+    uploadURL: 'https://appcontroller.next.aptugo.app/api/apps/upload?process=true&chunkName=tempfile.bin.part1&type=fe'
+  },
+  {
+    name: 'Upload Back-End',
     folders: ['back-end'],
+    useapi: true,
+    ignoreFolders: ['node_modules'],
     url: parameters.settings.apiURL.substring(8).toLowerCase(),
-    uploadURL: 'https://appuploader.aptugo.app:8501/upload?type=be'
+    uploadURL: 'https://appcontroller.next.aptugo.app/api/apps/upload?process=true&chunkName=tempfileback.bin.part1&type=be'
   }]
 }
 
